a53fbda18a89d4751fe7a328e39e2ae10eadbb89
robotra <john.f.obrienjr@gmail.com>
2021-08-17 12:22:08 -0400

    Update env_browser.js

--- env_browser.js
+++ env_browser.js
@@ -9,7 +9,7 @@ window.INFLUX_ENV = {
   /** InfluxDB v2 URL, '/influxdb' relies upon proxy to forward to the target influxDB */
   url: '/influx', //'http://localhost:8086',
   /** InfluxDB authorization token */
-  token: 'TyVKHPKlam2nL5kHIFIUTHK5bEY4ocBofYMrLmz00dAw6',
+  token: 'CoZ9B1jB0oEDTaSv5q3zqOKeJr0H9x_7pfUFNjGgZ5FRTj9zX-iqgQcZk2ZrXOpWs_OtzjZuWcEEvrMQnTB6gA==',
   /** InfluxDB organization */
   org: '48eb5fc1b0c3e9da',
   /** InfluxDB bucket used for onboarding and write requests. */
