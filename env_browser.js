b00b5c1aad3b0970aa336e55ee592a3830e634a6
robotra <john.f.obrienjr@gmail.com>
2023-10-17 07:22:00 -0400

    Update env_browser.js

--- env_browser.js
+++ env_browser.js
@@ -7,13 +7,13 @@
 // eslint-disable-next-line no-undef
 window.INFLUX_ENV = {
   /** InfluxDB v2 URL, '/influxdb' relies upon proxy to forward to the target influxDB */
-  url: 'https://us-west-2-1.aws.cloud2.influxdata.com/api/v2/', //'http://localhost:8086',
+  url: 'https://us-east-1-1.aws.cloud2.influxdata.com/api/v2/', //'http://localhost:8086',
   /** InfluxDB authorization token */
-  token: 'CoZ9B1jB0oEDTaSv5q3zqOKeJr0H9x_7pfUFNjGgZ5FRTj9zX-iqgQcZk2ZrXOpWs_OtzjZuWcEEvrMQnTB6gA==',
+  token: '5iq3ts97YXfRkGkhBTURznBf8P__I3NrtO275AsOO2du9UbUhz3s-pSHxGRvzq48moRg63_6SSCE40i5myXQhg==',
   /** InfluxDB organization */
-  org: '48eb5fc1b0c3e9da',
+  org: '9ce19a9d9b7af9ea',
   /** InfluxDB bucket used for onboarding and write requests. */
-  bucket: 'cfa20a86bf6081ca',
+  bucket: '659988312168c2a0',
 
   /** The following properties are used ONLY in the onboarding example */
   /** InfluxDB user  */
